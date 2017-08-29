2a700feaba34ccfd3cbd74183447ecd6f1e82e77
Julian Easterling <julian@julianscorner.com>
2017-08-29 08:20:21 -0400

    Added a failing test to implement the storage of Siege Jobs.

---
+++ UnitTests/JobControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using SiegeApi.Controllers;
+using SiegeApi.Data;
+using Xunit;
+
+namespace UnitTests
+{
+    [SuppressMessage(
+        "StyleCop.CSharp.DocumentationRules",
+        "SA1600:ElementsMustBeDocumented",
+        Justification = "Test Suites do not need XML Documentation.")]
+    public class JobControllerTests
+    {
+        [Fact]
+        public void Post_Should_StoreJob_When_GivenAValidJob()
+        {
+            // Arrange
+            var job = new Job();
+            job.AddUrl("http://localhost");
+
+            var controller = new JobController();
+
+            // Act
+            var id = controller.Post(job);
+            var storedJob = controller.Get().FirstOrDefault(x => x.Id == id);
+
+            // Assert
+            Assert.NotNull(storedJob);
+        }
+    }
+}
