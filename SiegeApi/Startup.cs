f771b0fbad07b64bbccf8a315ea50fac9ba316a9
Julian Easterling <julian@julianscorner.com>
2017-09-19 15:46:22 -0400

    Added the InMemory implementation of EF Core to Web API implementation...

--- SiegeApi/Startup.cs
+++ SiegeApi/Startup.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SiegeApi.Data;
 
 namespace SiegeApi
 {
@@ -24,13 +26,6 @@ namespace SiegeApi
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddMvc();
-        }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
@@ -38,6 +33,15 @@ namespace SiegeApi
             loggerFactory.AddDebug();
 
             app.UseMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddDbContext<JobContext>(options => options.UseInMemoryDatabase("SiegeApi"));
         }
     }
 }
