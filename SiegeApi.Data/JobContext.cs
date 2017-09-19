e669d32d26b863ed7eeac887032d470011b683db
Julian Easterling <julian@julianscorner.com>
2017-09-19 15:41:12 -0400

    Added a EF Core Context for the persistence layer...

---
+++ SiegeApi.Data/JobContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace SiegeApi.Data
+{
+    public class JobContext : DbContext
+    {
+        public JobContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
