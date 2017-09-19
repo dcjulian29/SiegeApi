44c45bc7644f73d7d2af08dc1225073e33747ba7
Julian Easterling <julian@julianscorner.com>
2017-09-19 15:45:31 -0400

    Due to the way EF Core operates, I put the List<string> into its own class and the created a List<T>... maybe a bug, maybe some different way at looking at the problem...

---
+++ SiegeApi.Data/Url.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiegeApi.Data
+{
+    /// <summary>
+    /// A class for EF Core that will end up being a list of URLs.
+    /// </summary>
+    public class Url
+    {
+        /// <summary>
+        /// Gets or sets the ID of this URL
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location of the URL.
+        /// </summary>
+        public string Location { get; set; }
+    }
+}
